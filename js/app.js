94677b1dee1695bb2c507c10ed840d401b68638b
NEELEY <aaronneeley@gmail.com>
2018-01-17 21:31:27 +0100

    hacky JQuery file

---
+++ js/app.js
@@ -0,0 +1,16 @@
+//$(document).foundation();
+//
+//$('a').click (function() {
+//    alert("a link was clicked");
+//});
+
+$(document).ready(function() {
+//      alert("document ready occurred!");
+    $('.cass').click (function() {
+        alert("cassy cass was clicked");
+        });
+    $('.knew').click (function() {
+        alert("KNEWTON was clicked");
+        });
+
+});
